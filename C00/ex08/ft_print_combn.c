4ef7a37bc3388c07833bc3cb8898daa3729228e9
mkurosaw <m.kurosawa.1995@gmail.com>
2022-01-05 20:51:29 +0900

    second commit

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -1,22 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkurosaw <mkurosaw@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/01/04 22:52:15 by mkurosaw          #+#    #+#             */
-/*   Updated: 2022/01/05 19:51:49 by mkurosaw         ###   ########.fr       */
+/*   Created: 2022/01/05 19:46:31 by mkurosaw          #+#    #+#             */
+/*   Updated: 2022/01/05 20:43:46 by mkurosaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 
-void	ft_print_comb(void);
+void	ft_print_comb2(void);
 
 int	main(void)
 {
-	ft_print_comb();
+	ft_print_comb2();
 	return (0);
 }
 
@@ -25,13 +25,12 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_print_result(int i, int j, int k, int is_delimiter)
+void	ft_print_result(int i, int j, int is_delimiter)
 {
-	if (i < j && j < k)
+	if (i < j)
 	{
-		ft_putchar('0' + i);
-		ft_putchar('0' + j);
-		ft_putchar('0' + k);
+		ft_putchar('0' + i / 10);
+		ft_putchar('0' + j % 10);
 		if (is_delimiter)
 		{
 			ft_putchar(',');
@@ -40,27 +39,22 @@ void	ft_print_result(int i, int j, int k, int is_delimiter)
 	}
 }
 
-void	ft_print_comb(void)
+void	ft_print_comb2(void)
 {
 	int	i;
 	int	j;
-	int	k;
 
 	i = 0;
-	while (i <= 7)
+	j = 1;
+	while (i <= 8)
 	{
-		j = 1;
-		while (i <= 8)
+		j = i + 1;
+		while (j <= 9)
 		{
-			k = 2;
-			while (i <= 9)
-			{
-				if (i == 7 && j == 8 && k == 9)
-					ft_print_result(i, j, k, 0);
-				else
-					ft_print_result(i, j, k, 1);
-				k++;
-			}
+			if (i == 8 && j == 9)
+				ft_print_result(i, j, 0);
+			else
+				ft_print_result(i, j, 1);
 			j++;
 		}
 		i++;
